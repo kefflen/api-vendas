5dc266aed667c53b28234a0c0bd12e54659e141a
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-01-25 21:20:18 -0300

    Middleware para tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import routes from './routes'
+import AppError from '../errors/AppError'
 
 const PORT = 4000
 const app = express()
@@ -8,9 +9,22 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
+app.use((error: Error, request: Request, response: Response, nextFunction: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Running at: http://localhost:${PORT}`)
 })
 
 
- 
