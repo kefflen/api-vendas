041955ec8d5f430710a87b40f37ae23862fd9438
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-01-27 21:25:19 -0300

    Configurando rotas de usuarios

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/products.routes'
+import usersRouter from '@modules/users/routes/users.routes'
 import { Router } from 'express'
 
 const routes = Router()
@@ -7,6 +8,6 @@ routes.get('/', (req, res) => {
   return res.json({message: 'hello dev'})
 })
 routes.use('/products', productsRouter)
-
+routes.use('/users', usersRouter);
 
 export default routes
