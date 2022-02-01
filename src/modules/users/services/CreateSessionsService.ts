4994f3fd1f59fc73b9ae7c71e4f9bc72057ee214
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-01-31 21:35:26 -0300

    Implementando JWT

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,20 @@ import { compare } from 'bcryptjs'
 import { getCustomRepository } from 'typeorm'
 import User from '../typeorm/entities/user'
 import UsersRepository from '../typeorm/repositories/UserRepository'
+import { sign } from 'jsonwebtoken'
 
 interface IRequest {
   email: string
   password: string
 }
 
+interface IResponse {
+  user: User
+  token: string
+}
+
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository)
     const user = await usersRepository.findByEmail(email)
 
@@ -23,8 +29,11 @@ class CreateSessionsService {
     if (!passwordConfirmed) {
       throw new AppError('Incorrect email/password combination.', 401)
     }
-
-    return user
+    const authsecrets = process.env.AUTHSECRETS
+    if (!authsecrets) throw new Error("Authsecrets key on enviroment variables not founded")
+    
+    const token = sign({}, authsecrets, {subject: user.id, expiresIn: '1d'})
+    return { user, token}
   }
 }
 
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/products.routes'
+import sessionsRouter from '@modules/users/routes/sessions.routes'
 import usersRouter from '@modules/users/routes/users.routes'
 import { Router } from 'express'
 
@@ -9,5 +10,5 @@ routes.get('/', (req, res) => {
 })
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter);
-
+routes.use('/sessions', sessionsRouter);
 export default routes
