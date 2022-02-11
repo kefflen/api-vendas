ce63847aac69863869f848d94783c90266af929d
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-02-10 21:20:49 -0300

    SendForgotPassword

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -22,8 +22,15 @@ class SendForgotPasswordEmailService {
     const token = await userTokensRepository.generate(user.id)
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token?.token}`
+      to: {name: user.name, email: user.email },
+      subject: '[API VENDAS] Recuperação de vendas',
+      templateData: {
+        template: `Ola {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token: token.token 
+        }
+      }
     });
   }
 }
